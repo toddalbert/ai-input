154d2638faa06cde3c5f0f78091bafde9605bacd
Todd Albert <todd.albert@siteimpact.com>
2024-04-18 11:43:18 -0400

    update to include two inputs

--- app/page.js
+++ app/page.js
@@ -1,74 +1,111 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 
 export default function Home() {
-  const [adText, setAdText] = useState()
-  const [loading, setLoading] = useState(false)
+  const [adText, setAdText] = useState();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    setAdText()
-    await setLoading(true)
-    const industry = e.target.elements[0].value
-    const prompt = `Generate a short and compelling ad for a company in the ${industry} industry. Only the body is needed.`
+    e.preventDefault();
+    setAdText();
+    await setLoading(true);
+    const industry = e.target.elements[0].value;
+    const product = e.target.elements[1].value;
+    const prompt = `Generate a short and compelling ad for a company in the ${industry} industry selling ${product}. Only the body is needed.`;
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}story`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ prompt }),
-    })
-    const data = await response.json()
-    await setAdText(data.text)
-    setLoading(false)
-  }
+    });
+    const data = await response.json();
+    await setAdText(data.text);
+    setLoading(false);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Want to try Gemini Pro?
+            Generate Ad Text
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
-            Choose an industry and we'll generate a short ad for you using Gemini Pro.
+            Choose an industry and we'll generate a short ad for you using
+            ✨Gemini Pro.
           </p>
           <form className="w-full max-w-md mx-auto" onSubmit={handleSubmit}>
-            <label
+            <div class="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+              <label
               for="industry"
               className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
             >
-              AI Test
+              Industry
             </label>
             <div className="relative">
               <input
                 type="text"
                 id="industry"
-                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Enter your industry here..."
+                className="block w-full p-4 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Industry"
                 required
               />
+            </div>
+              </div>
+              <div>
+              <label
+              for="product"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Product
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="product"
+                className="block w-full p-4 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Product"
+                required
+              />
+            </div>
+              </div>
+            </div>
               <button
                 type="submit"
-                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                ✨
+                ✨ Generate Ad Text ✨
               </button>
-            </div>
+
           </form>
           {loading && (
             <div class="mt-4 inline-flex justify-between items-center p-2 pe-4 mb-7 text-sm rounded bg-blue-900 text-blue-300 hover:bg-blue-800 animate-pulse">
-                <div role="status">
-                    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
-                    <span class="sr-only">Loading...</span>
-                </div>
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
             </div>
           )}
           {adText && (
             <p class="mt-4 inline-flex justify-between items-center p-2 pe-4 mb-7 text-sm rounded bg-blue-900 text-blue-300 hover:bg-blue-800">
-              <span class="text-sm font-medium">
-                {adText}
-              </span>
+              <span class="text-sm font-medium">{adText}</span>
               <svg
                 class="w-2.5 h-2.5 ms-2 rtl:rotate-180"
                 aria-hidden="true"
@@ -90,5 +127,5 @@ export default function Home() {
         <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
       </section>
     </main>
-  )
+  );
 }
